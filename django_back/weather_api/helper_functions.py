59d4315b186c91c03a2747974daf7ea043b9be8d
Chouia Abderrahman <chouia.abderrahman.ad@gmail.com>
2024-03-23 20:05:28 +0100

    Added helper functions for both getting coordinates and weather data from the api

--- django_back/weather_api/helper_functions.py
+++ django_back/weather_api/helper_functions.py
@@ -1,5 +1,8 @@
 from geopy.geocoders import Nominatim
-
+import openmeteo_requests
+import requests_cache
+import pandas as pd
+from retry_requests import retry
 
 def get_coordinates(location_name):
     geolocator = Nominatim(user_agent="my_geocoder")
@@ -12,11 +15,55 @@ def get_coordinates(location_name):
     else:
         return None, None
 
-#
-# # Example usage:
-location_name = "Annaba"
-latitude, longitude = get_coordinates(location_name)
-# if latitude is not None and longitude is not None:
-#     print(f"Latitude: {latitude}, Longitude: {longitude}")
-# else:
-#     print("Location not found or coordinates not available.")
+def get_weather_info(lat, lon, number_of_days=16):
+    # Setup the Open-Meteo API client with cache and retry on error
+    cache_session = requests_cache.CachedSession('.cache', expire_after=3600)
+    retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
+    openmeteo = openmeteo_requests.Client(session=retry_session)
+
+    # Make sure all required weather variables are listed here
+    # The order of variables in hourly or daily is important to assign them correctly below
+    url = "https://api.open-meteo.com/v1/cma"
+    params = {
+        "latitude": lat,
+        "longitude": lon,
+        "hourly": ["temperature_2m", "relative_humidity_2m", "dew_point_2m", "apparent_temperature", "precipitation",
+                   "rain", "showers", "snowfall", "snow_depth", "weather_code", "pressure_msl", "surface_pressure",
+                   "cloud_cover", "cloud_cover_low", "cloud_cover_mid", "cloud_cover_high", "visibility",
+                   "et0_fao_evapotranspiration", "vapour_pressure_deficit", "wind_speed_10m", "wind_speed_30m",
+                   "wind_speed_50m", "wind_speed_70m", "wind_speed_100m", "wind_speed_120m", "wind_speed_140m",
+                   "wind_speed_160m", "wind_speed_180m", "wind_speed_200m", "wind_direction_10m", "wind_direction_30m",
+                   "wind_direction_50m", "wind_direction_70m", "wind_direction_100m", "wind_direction_120m",
+                   "wind_direction_140m", "wind_direction_160m", "wind_direction_180m", "wind_direction_200m",
+                   "wind_gusts_10m", "surface_temperature", "soil_temperature_0_to_10cm", "soil_temperature_10_to_40cm",
+                   "soil_temperature_40_to_100cm", "soil_temperature_100_to_200cm", "soil_moisture_0_to_10cm",
+                   "soil_moisture_10_to_40cm", "soil_moisture_40_to_100cm", "soil_moisture_100_to_200cm", "is_day",
+                   "sunshine_duration", "cape", "lifted_index", "convective_inhibition"],
+        "timezone": "auto",
+        "forecast_days": number_of_days,
+    }
+    responses = openmeteo.weather_api(url, params=params)
+
+    # Process first location. Add a for-loop for multiple locations or weather models
+    response = responses[0]
+    print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
+    print(f"Elevation {response.Elevation()} m asl")
+    print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
+    print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
+
+    # Process hourly data directly without storing them in separate variables
+    hourly_data = {
+        "date": pd.date_range(
+            start=pd.to_datetime(response.Hourly().Time(), unit="s", utc=True),
+            end=pd.to_datetime(response.Hourly().TimeEnd(), unit="s", utc=True),
+            freq=pd.Timedelta(seconds=response.Hourly().Interval()),
+            inclusive="left"
+        )
+    }
+
+    # Assign hourly data directly to hourly_data dictionary
+    for i, variable_name in enumerate(params["hourly"]):
+        hourly_data[variable_name] = response.Hourly().Variables(i).ValuesAsNumpy()
+
+    hourly_dataframe = pd.DataFrame(data=hourly_data)
+    return hourly_dataframe
--- django_back/weather_api/models.py
+++ django_back/weather_api/models.py
@@ -1,3 +1,71 @@
 from django.db import models
 
-# Create your models here.
+class Locations(models.Model):
+
+    location_name = models.CharField(max_length=255, null=False)
+    def __str__(self):
+        return self.location_name
+
+class HourlyWeatherData(models.Model):
+
+    location_name = models.ForeignKey(Locations, on_delete=models.CASCADE)
+    date_time = models.DateTimeField(null=False)
+    temperature_2m = models.FloatField()
+    relative_humidity_2m = models.FloatField()
+    dew_point_2m = models.FloatField()
+    apparent_temperature = models.FloatField()
+    precipitation = models.FloatField()
+    rain = models.FloatField()
+    showers = models.FloatField()
+    snowfall = models.FloatField()
+    snow_depth = models.FloatField()
+    weather_code = models.FloatField()
+    pressure_msl = models.FloatField()
+    surface_pressure = models.FloatField()
+    cloud_cover = models.FloatField()
+    cloud_cover_low = models.FloatField()
+    cloud_cover_mid = models.FloatField()
+    cloud_cover_high = models.FloatField()
+    visibility = models.FloatField()
+    et0_fao_evapotranspiration = models.FloatField()
+    vapour_pressure_deficit = models.FloatField()
+    wind_speed_10m = models.FloatField()
+    wind_speed_30m = models.FloatField()
+    wind_speed_50m = models.FloatField()
+    wind_speed_70m = models.FloatField()
+    wind_speed_100m = models.FloatField()
+    wind_speed_120m = models.FloatField()
+    wind_speed_140m = models.FloatField()
+    wind_speed_160m = models.FloatField()
+    wind_speed_180m = models.FloatField()
+    wind_speed_200m = models.FloatField()
+    wind_direction_10m = models.FloatField()
+    wind_direction_30m = models.FloatField()
+    wind_direction_50m = models.FloatField()
+    wind_direction_70m = models.FloatField()
+    wind_direction_100m = models.FloatField()
+    wind_direction_120m = models.FloatField()
+    wind_direction_140m = models.FloatField()
+    wind_direction_160m = models.FloatField()
+    wind_direction_180m = models.FloatField()
+    wind_direction_200m = models.FloatField()
+    wind_gusts_10m = models.FloatField()
+    surface_temperature = models.FloatField()
+    soil_temperature_0_to_10cm = models.FloatField()
+    soil_temperature_10_to_40cm = models.FloatField()
+    soil_temperature_40_to_100cm = models.FloatField()
+    soil_temperature_100_to_200cm = models.FloatField()
+    soil_moisture_0_to_10cm = models.FloatField()
+    soil_moisture_10_to_40cm = models.FloatField()
+    soil_moisture_40_to_100cm = models.FloatField()
+    soil_moisture_100_to_200cm = models.FloatField()
+    is_day = models.BinaryField()
+    sunshine_duration = models.FloatField()
+    cape = models.FloatField()
+    lifted_index = models.FloatField()
+    convective_inhibition = models.FloatField()
+
+    def __str__(self):
+        return f"{self.location_name} {self.date_time}"
+    class Meta:
+        db_table = 'hourly_weather_data'
