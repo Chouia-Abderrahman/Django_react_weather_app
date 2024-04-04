69231647d2616d6143ee309bddf65baaa33bc2d8
Chouia Abderrahman <chouia.abderrahman.ad@gmail.com>
2024-04-04 01:38:34 +0100

    changing the way records are saved in db

--- django_back/weather_api/helper_functions.py
+++ django_back/weather_api/helper_functions.py
@@ -3,6 +3,8 @@ import openmeteo_requests
 import requests_cache
 import pandas as pd
 from retry_requests import retry
+import numpy as np
+from .models import HourlyWeatherData, Location
 
 def get_coordinates(location_name):
     geolocator = Nominatim(user_agent="my_geocoder")
@@ -46,10 +48,10 @@ def get_weather_info(lat, lon, number_of_days=16):
 
     # Process first location. Add a for-loop for multiple locations or weather models
     response = responses[0]
-    print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
-    print(f"Elevation {response.Elevation()} m asl")
-    print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
-    print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
+    # print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
+    # print(f"Elevation {response.Elevation()} m asl")
+    # print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
+    # print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
 
     # Process hourly data directly without storing them in separate variables
     hourly_data = {
@@ -63,10 +65,85 @@ def get_weather_info(lat, lon, number_of_days=16):
 
     # Assign hourly data directly to hourly_data dictionary
     for i, variable_name in enumerate(params["hourly"]):
-        hourly_data[variable_name] = response.Hourly().Variables(i).ValuesAsNumpy()
-
+        # hourly_data[variable_name] = response.Hourly().Variables(i).ValuesAsNumpy()
+        values = response.Hourly().Variables(i).ValuesAsNumpy()
+        values_without_nans = np.nan_to_num(values, nan=0.0)  # Replace NaNs with 0.0
+        hourly_data[variable_name] = values_without_nans
     hourly_dataframe = pd.DataFrame(data=hourly_data)
     hourly_dataframe['is_day'] = hourly_dataframe['is_day'].astype(int)
-    print(hourly_dataframe['is_day'])
-    hourly_dataframe.to_csv("test.csv")
     return hourly_dataframe
+
+def fetch_and_insert_weather_data_to_db(location_rec):
+    lat, lon = get_coordinates(location_rec.name)
+    weather_info_dataframe = get_weather_info(lat, lon)
+    print(len(weather_info_dataframe), weather_info_dataframe)
+    for _, row in weather_info_dataframe.iterrows():
+        # print("_",_)
+        # print("rowffffffffffffffff", row)
+        print("row['date_time']", row['date_time'])
+        hourly_weather_data = HourlyWeatherData()
+        hourly_weather_data.location_name = location_rec
+        hourly_weather_data.date_time = row['date_time']
+        hourly_weather_data.temperature_2m = row['temperature_2m']
+        hourly_weather_data.relative_humidity_2m = row['relative_humidity_2m']
+        hourly_weather_data.dew_point_2m = row['dew_point_2m']
+        hourly_weather_data.apparent_temperature = row['apparent_temperature']
+        hourly_weather_data.precipitation = row['precipitation']
+        hourly_weather_data.rain = row['rain']
+        hourly_weather_data.showers = row['showers']
+        hourly_weather_data.snowfall = row['snowfall']
+        hourly_weather_data.snow_depth = row['snow_depth']
+        hourly_weather_data.weather_code = row['weather_code']
+        hourly_weather_data.pressure_msl = row['pressure_msl']
+        hourly_weather_data.surface_pressure = row['surface_pressure']
+        hourly_weather_data.cloud_cover = row['cloud_cover']
+        hourly_weather_data.cloud_cover_low = row['cloud_cover_low']
+        hourly_weather_data.cloud_cover_mid = row['cloud_cover_mid']
+        hourly_weather_data.cloud_cover_high = row['cloud_cover_high']
+        hourly_weather_data.visibility = row['visibility']
+        hourly_weather_data.et0_fao_evapotranspiration = row['et0_fao_evapotranspiration']
+        hourly_weather_data.vapour_pressure_deficit = row['vapour_pressure_deficit']
+        hourly_weather_data.wind_speed_10m = row['wind_speed_10m']
+        hourly_weather_data.wind_speed_30m = row['wind_speed_30m']
+        hourly_weather_data.wind_speed_50m = row['wind_speed_50m']
+        hourly_weather_data.wind_speed_70m = row['wind_speed_70m']
+        hourly_weather_data.wind_speed_100m = row['wind_speed_100m']
+        hourly_weather_data.wind_speed_120m = row['wind_speed_120m']
+        hourly_weather_data.wind_speed_140m = row['wind_speed_140m']
+        hourly_weather_data.wind_speed_160m = row['wind_speed_160m']
+        hourly_weather_data.wind_speed_180m = row['wind_speed_180m']
+        hourly_weather_data.wind_speed_200m = row['wind_speed_200m']
+        hourly_weather_data.wind_direction_10m = row['wind_direction_10m']
+        hourly_weather_data.wind_direction_30m = row['wind_direction_30m']
+        hourly_weather_data.wind_direction_50m = row['wind_direction_50m']
+        hourly_weather_data.wind_direction_70m = row['wind_direction_70m']
+        hourly_weather_data.wind_direction_100m = row['wind_direction_100m']
+        hourly_weather_data.wind_direction_120m = row['wind_direction_120m']
+        hourly_weather_data.wind_direction_140m = row['wind_direction_140m']
+        hourly_weather_data.wind_direction_160m = row['wind_direction_160m']
+        hourly_weather_data.wind_direction_180m = row['wind_direction_180m']
+        hourly_weather_data.wind_direction_200m = row['wind_direction_200m']
+        hourly_weather_data.wind_gusts_10m = row['wind_gusts_10m']
+        hourly_weather_data.surface_temperature = row['surface_temperature']
+        hourly_weather_data.soil_temperature_0_to_10cm = row['soil_temperature_0_to_10cm']
+        hourly_weather_data.soil_temperature_10_to_40cm = row['soil_temperature_10_to_40cm']
+        hourly_weather_data.soil_temperature_40_to_100cm = row['soil_temperature_40_to_100cm']
+        hourly_weather_data.soil_temperature_100_to_200cm = row['soil_temperature_100_to_200cm']
+        hourly_weather_data.soil_moisture_0_to_10cm = row['soil_moisture_0_to_10cm']
+        hourly_weather_data.soil_moisture_10_to_40cm = row['soil_moisture_10_to_40cm']
+        hourly_weather_data.soil_moisture_40_to_100cm = row['soil_moisture_40_to_100cm']
+        hourly_weather_data.soil_moisture_100_to_200cm = row['soil_moisture_100_to_200cm']
+        hourly_weather_data.is_day = row['is_day']
+        hourly_weather_data.sunshine_duration = row['sunshine_duration']
+        hourly_weather_data.cape = row['cape']
+        hourly_weather_data.lifted_index = row['lifted_index']
+        hourly_weather_data.convective_inhibition = row['convective_inhibition']
+        # hourly_weather_data.location_name = location_
+
+        # print(weather_info_dataframe.columns)
+
+        # for col_name in weather_info_dataframe.columns:
+        #     setattr(hourly_weather_data, col_name, row[col_name])
+
+        saved = hourly_weather_data.save()
+        # print(saved)
--- django_back/weather_api/views.py
+++ django_back/weather_api/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import HourlyWeatherData, Location
-from .helper_functions import get_weather_info, get_coordinates
+from .helper_functions import get_weather_info, get_coordinates, fetch_and_insert_weather_data_to_db
 from datetime import datetime, date
 from .serializers import HourlyWeatherDataSerializer, LocationSerializer
 from django.utils import timezone
@@ -33,27 +33,24 @@ def current_day_weather(request):
 @api_view(['GET'])
 def current_day_weather_with_location(request, location):
 
-    location_rec = Location.objects.filter(name=location)
-
-    if not location_rec:
+    # location_rec = Location.objects.get(name=location)
+    try:
+        location_rec = Location.objects.get(name=location)
+    except Exception:
         location_rec = Location.objects.create(name=location)
         location_rec.save()
+        fetch_and_insert_weather_data_to_db(location_rec)
 
-        lat, lon = get_coordinates(location)
-        weather_info_dataframe = get_weather_info(lat, lon)
-        weather_info_dataframe['location_name'] = location_rec
+    if not HourlyWeatherData.objects.filter(location_name_id=location_rec.id, date_time__date=timezone.now().date()):
+        fetch_and_insert_weather_data_to_db(location_rec)
 
-        for _, row in weather_info_dataframe.iterrows():
-            hourly_weather_data = HourlyWeatherData()
-            for col_name in weather_info_dataframe.columns:
-                setattr(hourly_weather_data, col_name, row[col_name])
-
-            hourly_weather_data.save()
-
+    print(timezone.now().date())
     result_weather_data = HourlyWeatherData.objects.filter(
-        location_name=location_rec,
-        # date_time__date=timezone.now().date()
+        location_name_id=location_rec.id,
+        date_time__date=timezone.now().date()
     )
 
+    # print(result_weather_data)
     serializer = HourlyWeatherDataSerializer(result_weather_data, many=True)
+    # print(serializer.data)
     return Response(serializer.data)
